caf85cc353a9518725dd44667d53e5de966eb1e3
Kevin Minutti <minutti70@gmail.com>
2020-02-08 07:01:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
-def display_rainbow(colors)
-  puts  "R: #{colors[0],"
-end
-
 colors = ["red" , "orange" , "yellow" , "green" , "blue" , "indigo" , "violet"]
+
+def display_rainbow(colors)
+  puts  "R: #{colors[0]"
+end
 
 display_rainbow(colors)
