ad186169c7f15c8823caa22c8597ed43f4e9e224
Kevin Minutti <minutti70@gmail.com>
2020-02-08 06:50:50 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -4,4 +4,4 @@ def print_business_card(person)
 end
 bob     = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
-print_business_card(Bob)
+print_business_card(bob)
